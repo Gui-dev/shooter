fd29f77e70db02e0b627f5ce8972bde966cfb793
Gui-dev <adrianon10@gmail.com>
2022-07-25 03:14:16 -0300

    feat: add Global script

---
+++ scripts/Global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+var create_parent_node: Node = null
+
+
+func instance_node(node: PackedScene, location: Vector2, parent: Node2D) -> Node:
+  var node_instance = node.instance()
+  parent.add_child(node_instance)
+  node_instance.global_position = location  
+
+  return node_instance
