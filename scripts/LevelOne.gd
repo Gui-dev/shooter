421a96f4f68a8368bb39d24c77c674214482d91e
Gui-dev <adrianon10@gmail.com>
2022-07-26 02:52:07 -0300

    test: add player death and game restart

--- scripts/LevelOne.gd
+++ scripts/LevelOne.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
 
+var enemy_one = preload('res://scenes/Enemy.tscn')
+
+
 func _ready() -> void:
   Global.create_parent_node = self
   
 
 func _exit_tree() -> void:
   Global.create_parent_node = null
+
+
+func _on_timer_spawn_enemy_timeout() -> void:
+  var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 300))
+  
+  while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y > -45:
+    enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 300))
+  
+  Global.instance_node(enemy_one, enemy_position, self)
